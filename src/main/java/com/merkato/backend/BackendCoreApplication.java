bf3b67399ae094208c30f2d4be13e37ff3bae84a
Renato <renato.junior@hp.com>
2026-01-08 16:28:59 -0300

    test

--- src/main/java/com/merkato/backend/BackendCoreApplication.java
+++ src/main/java/com/merkato/backend/BackendCoreApplication.java
@@ -8,7 +8,6 @@ public class BackendCoreApplication {
 
     static void main(String[] args) {
         SpringApplication.run(BackendCoreApplication.class, args);
-        System.out.println("started");
     }
 
 }
